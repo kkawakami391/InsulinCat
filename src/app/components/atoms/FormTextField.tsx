9f0097f7fd4aae60d8d0f3b125b223a84b176172
kkawakami391 <kkawakami391@gmail.com>
2024-04-28 03:03:20 +0900

    Create the FormTextField component

---
+++ src/app/components/atoms/FormTextField.tsx
@@ -0,0 +1,114 @@
+import { Inputs } from "@/app/models/InputType";
+import { TextField, Typography } from "@mui/material";
+import React from "react";
+import { FieldErrors, UseFormRegister, Validate } from "react-hook-form";
+
+type Props = {
+  // Titulo del campo
+  title: string;
+
+  // Etiqueta del campo
+  label: string;
+
+  // Valor del campo
+  value?: string | number;
+
+  // Valor por defecto
+  defaultValue?: string | number;
+
+  // Tipo del campo
+  type?: "number" | "text";
+
+  // Funcion que se ejecuta al cambiar el valor del campo
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+  // Nombre de los campos del useForm
+  itemName: keyof Inputs;
+
+  // Funcion para registrar los campos del useForm
+  register: UseFormRegister<Inputs>;
+
+  // Si el campo es requerido
+  required?: boolean;
+
+  // Errores del useForm
+  errors: FieldErrors<Inputs>;
+};
+
+const FormTextField = (props: Props) => {
+  const {
+    title,
+    label,
+    value,
+    type = "text",
+    onChange,
+    itemName,
+    register,
+    required = false,
+    errors,
+  } = props;
+
+  // Validar que el numero sea positivo
+  const checkPositiveNumber: Validate<string | number | undefined, Inputs> = (
+    value
+  ) => {
+    // Si el valor es un numero y es menor a 0
+    if (type === "number" && typeof value === "number" && value <= 0) {
+      return "Tiene que ser un numero positivo!";
+    }
+    return true;
+  };
+
+  return (
+    <>
+      {type === "number" ? (
+        <>
+          <Typography variant="h6">{title}</Typography>
+          <TextField
+            label={label}
+            value={value}
+            variant="outlined"
+            color="secondary"
+            {...register(itemName, {
+              required: {
+                value: required,
+                message: "Este campo es requerido!",
+              },
+              valueAsNumber: true,
+              validate: { checkPositiveNumber },
+            })}
+            type={type}
+            sx={{ bgcolor: "white" }}
+          />
+          <Typography sx={{ color: "red" }}>
+            {errors[itemName]?.message}
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Typography variant="h6">{title}</Typography>
+          <TextField
+            label={label}
+            value={value}
+            variant="outlined"
+            color="secondary"
+            {...register(itemName, {
+              required: {
+                value: required,
+                message: "Este campo es requerido!",
+              },
+            })}
+            onChange={onChange}
+            type={type}
+            sx={{ bgcolor: "white" }}
+          />
+          <Typography sx={{ color: "red" }}>
+            {errors[itemName]?.message}
+          </Typography>
+        </>
+      )}
+    </>
+  );
+};
+
+export default FormTextField;
