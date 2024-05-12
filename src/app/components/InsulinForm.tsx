03734af9ff3f9ed60458d0eb1a7b636d3debb58d
kkawakami391 <kkawakami391@gmail.com>
2024-05-13 01:31:07 +0900

    feat: Add cat selection dropdown to InsulinForm

--- src/app/components/InsulinForm.tsx
+++ src/app/components/InsulinForm.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import { Button, Typography } from "@mui/material";
-import React, {  useState } from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import dayjs, { Dayjs } from "dayjs";
 import FormTextField from "./atoms/FormTextField";
 import { Inputs } from "../models/InputType";
 
-declare module "@mui/material/TextField" {
-  interface TextFieldPropsColorOverrides {
-    green: true;
-    brown: true;
-  }
-}
-
 declare module "@mui/material/Button" {
   interface ButtonPropsColorOverrides {
     green: true;
-    brown: true;
   }
 }
+// Api response type
+type catNamesData = {
+  name: string[];
+};
 
 const InsulinForm = () => {
   // Uso de useForm para manejar el formulario
@@ -32,29 +35,55 @@ const InsulinForm = () => {
   } = useForm<Inputs>();
 
   const today = dayjs();
-  const [catName, setCatName] = useState("Antifaz");
   const [value, setValue] = useState<Dayjs | null>(today);
+  const [catNames, setCatNames] = useState<string[]>([]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCatName(event.target.value);
-  };
+  // Funcion para obtener los nombres de los gatos
+  async function getCatsNames() {
+    const response = await fetch("http://localhost:3000/api/cats/get_cats");
+    const data: catNamesData = await response.json();
+    setCatNames(data.name);
+  }
+
+  // Uso de useEffect para obtener los nombres de los gatos al cargar el componente
+  useEffect(() => {
+    getCatsNames();
+  }, []);
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        <InputLabel color="secondary">Selecciona una gatinha</InputLabel>
+        <Controller
+          name="gatinha"
+          control={control}
+          defaultValue=""
+          rules={{ required: "Es necesario seleccionar una gatinha!" }}
+          render={({ field }) => (
+            <Select
+              {...field}
+              onChange={(e) => {
+                field.onChange(e);
+              }}
+              variant="outlined"
+              color="secondary"
+              label="Selecciona una gatinha"
+              sx={{ width: "223px", bgcolor: "white" }}
+            >
+              {catNames.map((catName) => (
+                <MenuItem key={catName} value={catName}>
+                  {catName}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+      </FormControl>
+      <Typography sx={{ color: "red" }}>{errors.gatinha?.message}</Typography>
       <FormTextField
-        title="Gatinha"
-        label="Gatinha"
-        value={catName}
-        onChange={handleChange}
-        itemName="gatinha"
-        register={register}
-        errors={errors}
-        required={true}
-      />
-      <FormTextField
-        title="Cantidad de jeringa utilizada:"
+        title="Cantidad de jeringas utilizadas:"
         label="Jeringas utilizadas"
         defaultValue={1}
         type="number"
@@ -70,7 +99,7 @@ const InsulinForm = () => {
         register={register}
         required={true}
         errors={errors}
-        />
+      />
       <FormTextField
         title="Cantidad de chuuru utilizadas:"
         label="Chuuru utilizadas"
@@ -78,7 +107,7 @@ const InsulinForm = () => {
         itemName="usedChuru"
         register={register}
         errors={errors}
-        />
+      />
       <Typography variant="h6">Dia de la inyeccion:</Typography>
       <Controller
         control={control}
