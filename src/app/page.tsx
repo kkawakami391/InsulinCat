2a347fc74b0b53894ae69a8593751f6945f750a9
kkawakami391 <kkawakami391@gmail.com>
2024-05-12 22:39:37 +0900

    Modify image tag property

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,14 @@ export default function Home() {
       <p>
         This is a simple app to track the insulin injections of a diabetic cat.
       </p>
-      <Image width={700} height={500} style={{borderRadius: "50px"}} src="/assets/gata_de_mierda.jpg" alt="gato de mier" />
+      <Image
+        width={700}
+        height={500}
+        style={{ borderRadius: "50px", marginBottom: "20px"}}
+        src="/assets/gata_de_mierda.jpg"
+        alt="gato de mier"
+        priority={true}
+      />
       <InsulinForm />
     </main>
   );
