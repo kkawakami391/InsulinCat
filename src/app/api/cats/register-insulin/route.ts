819cda3cac6474f8b73785506e71411f0df366fb
kkawakami391 <kkawakami391@gmail.com>
2024-05-19 19:16:10 +0900

    feat: Add API route to register insulin for cats

---
+++ src/app/api/cats/register-insulin/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/app/lib/prisma/db";
+import { parseDate } from "@/app/lib/dayjs/parseDate";
+
+type RequestInputs = NextRequest & {
+  gatinha: string;
+  vaccinLocation: string;
+  vaccinDate: string;
+  usedSyringe?: number;
+  usedChuru?: number;
+};
+
+export async function POST(req: RequestInputs): Promise<NextResponse> {
+  const data = await req.json();
+  const { gatinha, vaccinLocation, vaccinDate, usedSyringe, usedChuru } = data;
+  if (typeof vaccinLocation !== "string") {
+    return NextResponse.json({ error: "El tipo de input no es valido.", status: 400});
+  }
+  if (typeof vaccinDate !== "string") {
+    return NextResponse.json({ error: "El tipo de input no es valido.", status: 400 });
+  }
+  if (!usedSyringe) {
+    return NextResponse.json({
+      error: "Es necesario ingresar la cantidad de jeringas!", status: 400
+    });
+  }
+  if (usedSyringe < 1) {
+    return NextResponse.json({
+      error: "La cantidad de jeringas no puede ser menor a 1!", status: 400
+    });
+  }
+  if (usedChuru && usedChuru < 0) {
+    return NextResponse.json({
+      error: "La cantidad de chuuru no puede ser negativo!", status: 400
+    });
+  }
+
+  const catId = await prisma.cat.findFirst({
+    where: { name: gatinha },
+    select: { id: true },
+  });
+  if (!catId) {
+    return NextResponse.json({ error: "No se encontro el gato!", status: 404});
+  }
+
+  try {
+    const parsedJsDate = parseDate(vaccinDate);
+    await prisma.insulin.create({
+      data: {
+        vaccineLocation: vaccinLocation,
+        vaccinedAt: parsedJsDate,
+        syringeNum: usedSyringe,
+        chuuruNum: usedChuru,
+        cat: { connect: { id: catId.id}}
+      },
+    });
+    return NextResponse.json({ success: "Vacuna registrada con exito!", status: 200});
+  } catch (error) {
+    console.error("Error al registrar los datos", error);
+    return NextResponse.json({ error: "Database query failed", status: 500});
+  }
+}
