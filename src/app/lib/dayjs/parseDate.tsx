40b628f1adef187adf787436ad03a3320f21f342
kkawakami391 <kkawakami391@gmail.com>
2024-05-19 19:15:25 +0900

    feat: Add parseDate function for converting string to Date object

---
+++ src/app/lib/dayjs/parseDate.tsx
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+dayjs.extend(customParseFormat);
+
+// Esta funcion transofrma un string en formato "YYYY/MM/DD hh:mm A" a un objeto Date
+export const parseDate = (date: string) => {
+  const input = date;
+  const dayjsDate = dayjs(input, "YYYY/MM/DD hh:mm A");
+  const jsDate = dayjsDate.toDate();
+  return jsDate;
+};
