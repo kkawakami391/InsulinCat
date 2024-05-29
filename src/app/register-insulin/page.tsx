45beb43b2dfe6971aa58c83aac44a8846c592da9
kkawakami391 <kkawakami391@gmail.com>
2024-05-30 00:49:07 +0900

    feat: Add RegisterInsulin page with form and image

---
+++ src/app/register-insulin/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import InsulinForm from "../components/InsulinForm";
+
+export default function RegisterInsulin() {
+  return (
+    <main>
+      <h1>Welcome to GatitaInsulina</h1>
+      <p>
+        This is a simple app to track the insulin injections of a diabetic cat.
+      </p>
+      <Image
+        width={700}
+        height={500}
+        style={{ borderRadius: "50px", marginBottom: "20px" }}
+        src="/assets/gata_de_mierda.jpg"
+        alt="gato de mier"
+        priority={true}
+      />
+      <InsulinForm />
+    </main>
+  );
+}
