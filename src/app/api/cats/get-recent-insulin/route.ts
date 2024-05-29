56430ab7d69dc3ddf1d5c8266266679175674a8b
kkawakami391 <kkawakami391@gmail.com>
2024-05-30 00:39:19 +0900

    feat: Add API route to get recent insulin data for cats

---
+++ src/app/api/cats/get-recent-insulin/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma/db";
+import { dateToString } from "@/app/lib/dayjs/parseDate";
+
+type RequestBodyType = {
+  catName: string;
+};
+
+export async function POST(req: NextRequest) {
+  const data: RequestBodyType = await req.json();
+  try {
+    const selectedCatId = await prisma.cat.findFirst({
+      where: {
+        name: data.catName,
+      },
+      select: {
+        id: true,
+      },
+    });
+    // Obtener los datos de las vacunas con el id del gato seleccionado
+    if (selectedCatId) {
+      const recentInsulinData = await prisma.insulin.findMany({
+        where: {
+          catId: selectedCatId.id,
+        },
+        select: {
+          vaccineLocation: true,
+          vaccinedAt: true,
+          chuuruNum: true,
+        },
+        orderBy: {
+          // Ordenar por fecha de vacunación de forma descendente
+          vaccinedAt: "desc",
+        },
+        // Obtener solo 5 datos
+        take: 5,
+      });
+      // TODO: Transformar los datos para que puedan utlizarse en el front. (vaccinedAt a string y chuuruNum a number)
+      const recentInsulinDataFormatted = recentInsulinData.map((insulin) => {
+        return {
+          vaccineLocation: insulin.vaccineLocation,
+          vaccinedAt: dateToString(insulin.vaccinedAt),
+          chuuruNum: insulin.chuuruNum?.toNumber(),
+        };
+      });
+      return NextResponse.json(recentInsulinDataFormatted, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Cat not found", status: 404 });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
