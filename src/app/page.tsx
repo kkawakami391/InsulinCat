8590aa25cdb5c0952a800da1289707298ea7069a
kkawakami391 <kkawakami391@gmail.com>
2024-05-29 23:38:43 +0900

    Feature: Add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
-import Image from "next/image";
-import InsulinForm from "./components/InsulinForm";
+"use client";
+
+import { Box, Button, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/register-insulin");
+  };
+
   return (
-    <main>
-      <h1>Welcome to GatitaInsulina</h1>
-      <p>
-        This is a simple app to track the insulin injections of a diabetic cat.
-      </p>
-      <Image
-        width={700}
-        height={500}
-        style={{ borderRadius: "50px", marginBottom: "20px"}}
-        src="/assets/gata_de_mierda.jpg"
-        alt="gato de mier"
-        priority={true}
-      />
-      <InsulinForm />
-    </main>
+    <Box>
+      <Typography variant="h2" align="center">
+        Welcome to Gatita Insulina!
+      </Typography>
+      <Box>
+        <Button
+          variant="contained"
+          color="green"
+          sx={{ my: 3, display: "flex", marginX: "auto"}}
+          onClick={handleClick}
+        >
+          Enter to site!
+        </Button>
+      </Box>
+    </Box>
   );
 }
